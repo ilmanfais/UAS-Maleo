faae8d60291dab0fbfb26bde6f14c085e25781eb
M. FASRUL FAIS ILMAN <ilmanfaisocu26@gmail.com>
2023-11-04 20:56:54 +0700

    kuyy

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import ScrollToTop from './components/ScrollToTop.jsx';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {  } from "./dist/css/main.css";
+import {  } from "./data/main.css";
 import 'animate.css';
 
 import AOS from 'aos';
